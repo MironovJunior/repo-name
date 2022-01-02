7c4d96b2cbbe610049957603b7e29015d6e7530c
Миронов <91536898+MironovJunior@users.noreply.github.com>
2022-01-02 13:28:01 +0300

    Add files via upload

---
+++ Практика по веб-разработке/js/script.js
@@ -0,0 +1,14 @@
+function redirect1(){window.location = 'https://www.figma.com/file/iuOVqJJXXgFapdeBv0a38A/Cornelia-Template?node-id=173%3A216.html';}
+function redirect2(){window.location = 'https://www.sketch.com/.html';}
+
+function View() {
+    document.getElementById("pop_up").style.display = "block";
+}
+
+function Close(){
+    document.getElementById("pop_up").style.display = "none";
+}
+
+
+			
+		
